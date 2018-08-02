c8424bd4bf3ff683ad286ce20142ac93d95242b4
Gál Péter <pepyta118@gmail.com>
2018-08-02 16:33:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -16,16 +16,19 @@ client.on('message', msg => {
 
 });
 
-
-client.on('message', async message => {
+client.on('message', message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
   if (!message.guild) return;
 
-  if (message.content === 'itt vagy?') {
+  if (message.content === '/join') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
-      const connection = await message.member.voiceChannel.join();
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
     } else {
       message.reply('You need to join a voice channel first!');
     }
