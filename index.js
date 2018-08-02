ddfdbeb629e8f644e288bc5420fadcaed7006852
Gál Péter <pepyta118@gmail.com>
2018-08-02 16:03:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Logged in as ${client.user.tag}!");
+  client.user.setStatus('dnd', 'Made by pepyta');
 });
 
 client.on('message', msg => {
