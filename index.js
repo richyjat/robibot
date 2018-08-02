637a400720086357606cec00ce612cf553d29152
Gál Péter <pepyta118@gmail.com>
2018-08-02 16:13:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   
-client.user.setPresence({ game: { name: 'with pepyta\'s duck', type: 0 } });
+client.user.setPresence({ game: { name: 'with pepyta\'s duck', type: 0 } })
   .then(console.log)
   .catch(console.error);
 });
