e157e8047ae5b55049968d16ce4b96e9a0e9ce8c
Gál Péter <pepyta118@gmail.com>
2018-08-02 15:53:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// Discord.js bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-client.login(process.env.TOKEN);
+client.login('NDc0NTA1MTMzMjQyMzE4ODQ5.DkSd6g.WT6zQO5nKCQ54AOGXxND4ELwEjU');
