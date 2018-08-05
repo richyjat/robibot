ff4b8efe7b3c1e3367ff72f45d0c03219a025bfa
Gál Péter <pepyta118@gmail.com>
2018-08-05 21:29:50 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -34,38 +34,6 @@ client.on('message', msg => {
     }
 });
 
-client.on('message', message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-
-    if (message.content === '/join') {
-        // Only try to join the sender's voice channel if they are in one themselves
-        if (message.member.voiceChannel) {
-            // check if there is already a voice connection
-            if (message.member.voiceChannel.connection) {
-                console.log('conn status: ' + message.member.voiceChannel.connection.status);
-            }
-            console.log('is joinable: ' + message.member.voiceChannel.joinable);
-            if (message.member.voiceChannel.joinable) {
-                message.member.voiceChannel.join()
-                    .then(connection => { // Connection is an instance of VoiceConnection
-                        message.reply('I have successfully connected to the channel!');
-                        const dispatcher = connection.playFile('./itt_vagyok.mp3');
-                        dispatcher.on('end', () => {
-                            // The song has finished
-                        });
-                    })
-                    .catch(console.error);
-            }
-        } else {
-            message.reply('You need to join a voice channel first!');
-        }
-    }
-});
-
-
-
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel
     let oldUserChannel = oldMember.voiceChannel
@@ -73,22 +41,24 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 
     if (oldUserChannel === undefined && newUserChannel !== undefined) {
 
-
-        if (newMember.voiceChannel) {
-            // check if there is already a voice connection
-            if (newMember.voiceChannel.connection) {
-                console.log('conn status: ' + newMember.voiceChannel.connection.status);
-            }
-            console.log('is joinable: ' + newMember.voiceChannel.joinable);
-            if (newMember.voiceChannel.joinable) {
-                newMember.voiceChannel.join()
-                    .then(connection => { // Connection is an instance of VoiceConnection
-                        const dispatcher = connection.playFile('./itt_vagyok.mp3');
-                        dispatcher.on('end', () => {
-                            // The song has finished
-                        });
-                    })
-                    .catch(console.error);
+        if (isReady) {
+            if (newMember.voiceChannel) {
+                isReady = false;
+                // check if there is already a voice connection
+                if (newMember.voiceChannel.connection) {
+                    console.log('conn status: ' + newMember.voiceChannel.connection.status);
+                }
+                console.log('is joinable: ' + newMember.voiceChannel.joinable);
+                if (newMember.voiceChannel.joinable) {
+                    newMember.voiceChannel.join()
+                        .then(connection => { // Connection is an instance of VoiceConnection
+                            const dispatcher = connection.playFile('./itt_vagyok.mp3');
+                            dispatcher.on('end', () => {
+                                isReady = true;
+                            });
+                        })
+                        .catch(console.error);
+                }
             }
         }
 
